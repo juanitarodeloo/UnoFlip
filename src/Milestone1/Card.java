8f9b8a5e27da7c6e9a0986f7316c97bda75544b6
user <kmapkco@gmail.com>
2023-10-15 10:51:26 -0400

    card class with attributes and methods

--- src/Milestone1/Card.java
+++ src/Milestone1/Card.java
@@ -1,4 +1,43 @@
 package Milestone1;
 
 public class Card {
+
+    public enum Color{
+        RED, YELLOW, GREEN, BLUE, PINK, TEAL, ORANGE, PURPLE,
+    }
+
+    public enum Type{
+        REVERSE, SKIP, WILD, WILD_DRAW_TWO
+
+        }
+
+    private Color color;
+    private Type type;
+
+    public Card(Color color, Type type){
+        this.color = color;
+        this.type = type;
+
+    }
+
+    public Color getColor(){
+        return color;
+
+    }
+    public void setColor(){
+        this.color = color;
+    }
+    public Type getType(){
+        return type;
+    }
+
+    public void setType(){
+        this.type = type;
+    }
+    public String toString(){
+        return color + " " + type;
+    }
+
+
+
 }
